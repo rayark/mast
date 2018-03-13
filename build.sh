6459d650a7998c777ce958d0a3497cb6b14226a5
Alvin Chung <alvin@rayark.com>
2018-03-13 15:45:57 +0800

    create build folder

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
- mcs -langversion:4 -sdk:2 -target:library -out:Rayark.Mast.DLL $(find Assets/Plugins/Rayark/Mast -path Assets/Plugins/Rayark/Mast/Editor -prune -o -name "*.cs" -print)
+ mcs -langversion:4 -sdk:2 -target:library -out:build/Assets/Plugins/Rayark/Mast/Rayark.Mast.DLL $(find Assets/Plugins/Rayark/Mast -path Assets/Plugins/Rayark/Mast/Editor -prune -o -name "*.cs" -print)
